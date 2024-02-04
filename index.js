808efe07317f6d92676f7279e6d7081c9525bf08
muhammadsadri19 <muhammadsadry19@gmail.com>
2024-02-04 10:39:11 +0700

    CRUD-API

--- index.js
+++ index.js
@@ -9,26 +9,82 @@ const response = require('./response')
 app.use(bodyParser.json())
 
 
-app.get('/users', (req, res) => {
-    const querySql = "SELECT * FROM tb_mahasiswa"
-  db.query(querySql, (error, result) => {
-    response(200, result, "Semua data mahasiswa", res)
+app.get('/', (req, res) => {
+  response(200, 'KUMPULAM DIGISTAR API', 'SUCCESS', res)
+})
+
+app.get('/list-mahasiswa', (req, res) => {
+  const sql = 'SELECT * FROM tb_mahasiswa'
+  db.query(sql, (err, result) => {
+    if (err) throw err 
+    response(200, result, 'GET-API-LIST-MAHASISWA.', res)
   })
 })
 
-app.post('/login', (req, res) => {
-    console.log({
-        requstFromOutside : req.body
-    })
-    res.send('Anda sudah login')
+app.get('/list-mahasiswa/:id', (req, res) => {
+  const id = req.params.id
+  const sql = `SELECT * FROM tb_mahasiswa WHERE id = ${id}`
+  db.query(sql, (err, result) => {
+    if (err) throw err
+    response(200, result, 'GET-API-LIST-MAHASISWA.', res)
+  })
 })
 
-app.put('/username', (req, res) => {
-    console.log({
-        updateData : req.body
-    })
-    res.send('Data sudah update')
+app.post('/list-mahasiswa', (req, res) => {
+  const {name, email, phone_number, major, campus} = req.body
+  const sql = `INSERT INTO tb_mahasiswa (name, email, phone_number, major, campus) VALUES ('${name}','${email}','${phone_number}','${major}','${campus}')`
+
+  db.query(sql, (err, result) => {
+    if(err) response(500, 'invalid', 'error', res)
+    if (result?.affectedRows){
+      response(200, result.insertId, 'Data Mahasiswa Baru Telah Ditambahkan', res)
+    } 
+  })
 })
+
+
+app.put('/list-mahasiswa', (req, res) => {
+  const {id, name, email, phone_number, major, campus} = req.body
+  const sql = `UPDATE tb_mahasiswa SET id='${id}',name='${name}',email='${email}',phone_number='${phone_number}',major='${major}',campus='${campus}' WHERE id = ${id}`
+
+  db.query(sql, (err, result) => {
+    
+    if (err) response(500, 'invalid', 'error', res)
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess : result.affectedRows,
+        message : result.message
+      }
+      response(200, data, 'Update Data Success', res)
+    }else{
+      response(404, 'Mahasiswa tidak ditemukan', 'ERROR', res)
+    }
+    
+  })
+
+})
+
+app.delete('/list-mahasiswa', (req, res) => {
+  const {id} = req.body
+  const sql = `DELETE FROM tb_mahasiswa WHERE id = ${id}`
+
+  db.query(sql, (err, result) => {
+    if (err) response(500, 'invalid', 'error', res)
+    if (result?.affectedRows) {
+      const data = {
+        isSuccess : result.affectedRows,
+        message : "Data sudah dihapus"
+      }
+      response(200, "Berhasil hapus data", "SUCCESS", res)
+    }else{
+      response(500, "Data tidak ditemukan", "ERROR", res)
+    }
+    
+  })
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
